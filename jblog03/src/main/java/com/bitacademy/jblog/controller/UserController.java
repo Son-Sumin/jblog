1bc24360ec0e0de0c312178522218ec6247704ed
Son-Sumin <goldkitty007@gmail.com>
2022-11-29 21:14:03 +0900

    jblog03

--- jblog03/src/main/java/com/bitacademy/jblog/controller/UserController.java
+++ jblog03/src/main/java/com/bitacademy/jblog/controller/UserController.java
@@ -1,23 +1,31 @@
 package com.bitacademy.jblog.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.bitacademy.jblog.service.UserService;
+import com.bitacademy.jblog.vo.UserVo;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
+	@Autowired
+	private UserService userService;
 
-	@RequestMapping("/user/join")
+	@RequestMapping(value="/join", method=RequestMethod.GET)
 	public String join() {
 		return "user/join";
 	}
 	
-	@RequestMapping("/user/joinsuccess")
-	public String joinsuccess() {
-		return "user/joinsuccess";
+	@RequestMapping(value="/join", method=RequestMethod.POST)
+	public String joinsuccess(UserVo userVo) {
+		userService.join(userVo);
+		return "redirect:/user/joinsuccess";
 	}
 	
-	@RequestMapping("/user/login")
+	@RequestMapping("/login")
 	public String login() {
 		return "user/login";
 	}
--- jblog03/src/main/java/com/bitacademy/jblog/service/UserService.java
+++ jblog03/src/main/java/com/bitacademy/jblog/service/UserService.java
@@ -4,9 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.bitacademy.jblog.repository.UserRepository;
+import com.bitacademy.jblog.vo.UserVo;
 
 @Service
 public class UserService {
 	@Autowired
 	private UserRepository userRepository;
+
+	public void join(UserVo userVo) {
+		
+	}
 }
