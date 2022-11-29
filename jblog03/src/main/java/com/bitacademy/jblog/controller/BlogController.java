b47fb2f6ccf7a24b540965e12357fbdb32293427
Son-Sumin <goldkitty007@gmail.com>
2022-11-29 16:50:26 +0900

    jblog03

--- jblog03/src/main/java/com/bitacademy/jblog/controller/BlogController.java
+++ jblog03/src/main/java/com/bitacademy/jblog/controller/BlogController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/{id:(?!assets).*}")  // 없어도되고 있어도되고 assets으로 시작하는 모든 것
+@RequestMapping("/{id:(?!assets).*}")  // assets 없어도되고 있어도되고 assets으로 시작하는 모든 것
 public class BlogController {
 
 	@RequestMapping({"", "/{pathNo1}", "/{pathNo1}/{pathNo2}"})
@@ -19,12 +19,13 @@ public class BlogController {
 		Long postNo = 0L;
 		
 		if(pathNo1.isPresent()) {
-			categoryNo = pathNo2.get();
+			categoryNo = pathNo1.get();
 		} else if(pathNo2.isPresent()) {
 			categoryNo = pathNo1.get();
 			postNo = pathNo2.get();
 		} else {
 			// 디폴트
+			return"blog/index";
 		}
 		
 		return"blog/index";
