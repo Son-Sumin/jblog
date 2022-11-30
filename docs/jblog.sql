0cc11cb894dcbbc5c7d72a74cf4d9d1c53e7dc07
Son-Sumin <goldkitty007@gmail.com>
2022-11-30 09:29:14 +0900

    jblog03

--- docs/jblog.sql
+++ docs/jblog.sql
@@ -3,3 +3,13 @@ desc user;
 desc blog;
 desc category;
 desc post;
+
+-- USER----------------------------
+-- insert
+select * from user;
+
+delete from user where name='zzanggu';
+
+insert into user values('zzanggu', '짱구', '1234');
+insert into user values('menggu', '맹구', '1234');
+insert into user values('zzanggu', '짱구', '1234');
