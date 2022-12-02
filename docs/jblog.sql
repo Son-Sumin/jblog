8d4e69b490275bb453ea25fa134d4627b20b4fc4
Son-Sumin <goldkitty007@gmail.com>
2022-12-02 21:35:47 +0900

    jblog03

--- docs/jblog.sql
+++ docs/jblog.sql
@@ -5,12 +5,25 @@ desc category;
 desc post;
 
 -- USER----------------------------
+alter table user add column role enum('user', 'admin') default 'user';
+
 -- insert
 select * from user;
-alter table user add column role enum('user', 'admin') default 'user';
 
 delete from user where name='맹구';
 
 insert into user values('zzanggu', '짱구', '1234', 'user');
 insert into user values('menggu', '맹구', '1234', 'admin');
 insert into user values('chulsu', '철수', '1234', 'user');
+
+-- select
+select id, name, role
+  from user
+ where id = 'menggu'
+   and password = '1234';
+
+
+-- BLOG----------------------------
+select * from blog; 
+
+insert into blog values('menggu', '맹구의블로그', 'profile');
